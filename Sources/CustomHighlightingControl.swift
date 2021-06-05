6a1da9564017c1b2c1ee604c9804fd8fe9cf8919
Andreas Verhoeven <ave@aveapps.com>
2021-06-05 17:41:04 +0200

    Added CustomHighlightingControl

---
+++ Sources/CustomHighlightingControl.swift
@@ -0,0 +1,94 @@
+//
+//  CustomHighlightingControl.swift
+//  AveCommonViews
+//
+//  Created by Andreas Verhoeven on 05/06/2021.
+//
+
+import UIKit
+
+/// A UIControl subclass that does highlighting like UIButton does it
+public class CustomHighlightingControl: UIControl {
+
+
+	public enum HighlightingStyle {
+		/// doesn't highlight on initial touch down
+		case normal
+
+		/// always animates, even on touch down
+		case alwaysAnimate
+	}
+
+	/// how we should highlight
+	open var highlightingStyle: HighlightingStyle = .alwaysAnimate
+
+	public typealias HighlightingCallback = (CustomHighlightingControl) -> Void
+
+	/// called by updateHighlighting by default
+	open var highlightingCallback: HighlightingCallback?
+
+	/// Default fades out the view
+	open func updateHighlighting(_ shouldHighlight: Bool) {
+		highlightingCallback?(self)
+	}
+
+	open func setIsHighlighted(_ highlighted: Bool, animated: Bool) {
+		guard isHighlighted != highlighted else { return }
+		isHighlighted = highlighted
+
+		let updates = {
+			self.updateHighlighting(self.isHighlighted)
+		}
+		if animated == true {
+			UIView.animate(withDuration: 0.25, delay: 0, options: [.beginFromCurrentState, .allowUserInteraction], animations: updates)
+		} else {
+			updates()
+		}
+	}
+
+	// MARK: - Input
+	@objc private func customButtonTouchesChanged(_ sender: Any, event: UIEvent?) {
+		let shouldBeHighlighted = isTracking && isTouchInside
+		let touchPhase = event?.allTouches?.first?.phase ?? .began
+
+		if touchPhase == .began && highlightingStyle == .normal {
+			updateHighlighting(shouldBeHighlighted)
+		} else {
+			var options: UIView.AnimationOptions = .allowUserInteraction
+			if touchPhase  != .ended {
+				options.insert(.beginFromCurrentState)
+			}
+
+			UIView.animate(withDuration: 0.25, delay: 0, options: options) {
+				self.updateHighlighting(shouldBeHighlighted)
+			}
+		}
+	}
+
+	// MARK: - Private
+	private func setup() {
+		addTouchesHandlerIfNeeded()
+	}
+
+	private func addTouchesHandlerIfNeeded() {
+		guard (actions(forTarget: self, forControlEvent: .touchDown)?.contains(NSStringFromSelector(#selector(customButtonTouchesChanged(_:event:)))) ?? false) == false else { return }
+		addTarget(self, action: #selector(customButtonTouchesChanged(_:event:)), for: .allTouchEvents)
+	}
+
+	// MARK: - UIControl
+	open override func removeTarget(_ target: Any?, action: Selector?, for controlEvents: UIControl.Event) {
+		super.removeTarget(target, action: action, for: controlEvents)
+		addTouchesHandlerIfNeeded()
+	}
+
+	// MARK: - UIView
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		setup()
+	}
+
+	required init?(coder: NSCoder) {
+		super.init(coder: coder)
+		setup()
+	}
+}
