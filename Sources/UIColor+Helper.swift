bf9122e82577151e81671ab6447a262abb664dfb
Andreas Verhoeven <ave@aveapps.com>
2022-07-03 22:46:23 +0200

    Added blend function

--- Sources/UIColor+Helper.swift
+++ Sources/UIColor+Helper.swift
@@ -35,4 +35,31 @@ public extension UIColor {
 	
 	/// Returns the light variant of this color
 	var alwaysLight: UIColor { withInterfaceStyle(.light) }
+	
+	/// Blends this color with another color in a simple way. Works with trait collections.
+	func blended(with other: UIColor) -> UIColor {
+		func blended(first: UIColor, second: UIColor) -> UIColor {
+			var (r1, g1, b1, a1): (CGFloat, CGFloat, CGFloat, CGFloat) = (0, 0, 0, 0)
+			first.getRed(&r1, green: &g1, blue: &b1, alpha: &a1)
+			
+			var (r2, g2, b2, a2): (CGFloat, CGFloat, CGFloat, CGFloat) = (0, 0, 0, 0)
+			second.getRed(&r2, green: &g2, blue: &b2, alpha: &a2)
+			
+			let outA = a1 + a2 * (1 - a1)
+			guard outA > 0 else { return UIColor(white: 0, alpha: 0) }
+			
+			let outR = (r1 * a1 + r2 * a2 * (1 - a1)) / outA
+			let outG = (g1 * a1 + g2 * a2 * (1 - a1)) / outA
+			let outB = (b1 * a1 + b2 * a2 * (1 - a1)) / outA
+			return UIColor(red: outR, green: outG, blue: outB, alpha: outA)
+		}
+		
+		if #available(iOS 13, *) {
+			return withDynamicProvider { color, traitCollection in
+				blended(first: color.resolvedColor(with: traitCollection), second: other.resolvedColor(with: traitCollection))
+			}
+		} else {
+			return blended(first: self, second: other)
+		}
+	}
 }
